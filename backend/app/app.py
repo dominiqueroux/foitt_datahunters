2f9b69e5e83f760a18f90f1ed1d129a68745687e
Jan Seitz <jan.seitz@me.com>
2021-09-25 14:39:50 +0200

    populate db with existing data on docker build

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,6 +5,8 @@ from werkzeug.utils import cached_property
 import flask
 from flask import request
 
+import sqlalchemy
+
 flask.helpers._endpoint_from_view_func = _endpoint_from_view_func
 
 werkzeug.cached_property = cached_property
@@ -23,10 +25,36 @@ class HelloWorld(Resource):
 @api.route("/ask", methods=['GET'])
 class AskToValueNet(Resource):
     def get(self):
-        if 'id' in request.args:
-            id = int(request.args['id'])
-        else:
-            return "Error: No id field provided. Please specify an id."
+        # if 'id' in request.args:
+        #     id = int(request.args['id'])
+        # else:
+        #     return "Error: No id field provided. Please specify an id."
+
+        DB_USER = 'hack_zurich'
+        DB_PW = 'hack_zurich'
+        DB_HOST = 'postgres'
+        DB_PORT = '5432'
+        DB_SCHEMA = 'hack_zurich'
+        DB = 'hack_zurich'
+        DRIVERNAME = "postgresql"
+
+        engine = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL(
+                drivername=DRIVERNAME,
+                username=DB_USER,
+                password=DB_PW,
+                host=DB_HOST,
+                port=DB_PORT,
+                database=DB,
+            ),
+            echo_pool=True,
+        )
+        print("connecting with engine " + str(engine))
+        connection = engine.connect()
+
+        resultproxy = connection.execute("SELECT * FROM commune_type")
+
+        return [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy]
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
