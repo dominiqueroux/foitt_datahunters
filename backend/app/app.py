806ddb9a267e789bf1817c541ef3b7f118b34a56
Jan Seitz <jan.seitz@me.com>
2021-09-25 22:53:20 +0200

    provide graph data

--- backend/app/app.py
+++ backend/app/app.py
@@ -9,6 +9,7 @@ from flask import request
 import sqlalchemy
 import requests
 import json
+import re
 
 flask.helpers._endpoint_from_view_func = _endpoint_from_view_func
 
@@ -68,16 +69,36 @@ class AskToValueNet(Resource):
         else:
             question = "What is the share of electric cars in 2016 for Wetzikon?"
 
-
+         # Get SQL query
         payload = {'question': question}
         headers = {'Content-Type': 'application/json', "X-API-KEY": "sjNmaCtviYzXWlS"}
         r = requests.put("https://inference.hackzurich2021.hack-with-admin.ch/api/question/hack_zurich", headers=headers, data=json.dumps(payload)) 
 
         if r.status_code == 200:
-            resultproxy = connection.execute(r.json()["sql"])
-            response = flask.jsonify([{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy])
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
+            response = {}
+
+            # Get the «standard» data
+            sql_statement = r.json()["sql"]
+            resultproxy = connection.execute(sql_statement)
+            standard_data = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy]
+            response["standard_data"] = standard_data
+
+            # Calculate extended query
+            #sql_statement = "SELECT T1.share_electric_cars FROM share_electric_cars AS T1 JOIN spatialunit AS T2 ON T1.spatialunit_id = T2.spatialunit_id WHERE T1.year = 2016 and T2.name = 'Wetzikon'       "
+            m = re.search('(SELECT )([a-zA-Z1-9._]+)( FROM )', sql_statement)
+            select_column = m.group(2)
+            splitted_query = [string.split("FROM") for string in sql_statement.split("WHERE")]
+            middle_query = splitted_query[0][1]
+            where_clause = splitted_query[1][0].split(" and ")[1]
+            year_column = splitted_query[1][0].split(" and ")[0].split(" = ")[0]
+            new_query = "SELECT AVG("+select_column+"), "+year_column+" FROM "+middle_query+" WHERE "+where_clause+" GROUP BY "+year_column
+
+            resultproxy2 = connection.execute(new_query)
+            further_information = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy2]
+            response["further_information"] = further_information
+            json_response = flask.jsonify(response)
+            json_response.headers.add('Access-Control-Allow-Origin', '*')
+            return json_response
 
         else:
             print("error", r.status_code)
