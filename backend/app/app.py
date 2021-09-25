256dfa9835559bd214e35e9c0763c5b67d9264b6
Roland Hediger <me@rolandh.dev>
2021-09-25 13:49:39 +0200

    Docker compose (#1)
    * add docker compose file
    
    * Add link to postgres DB, remove valuenet
    
    * add database dump
    
    * code with volume
    
    * code with volume
    
    * code with volume
    
    * code with volume
    
    * add new method ask
    
    Co-authored-by: Jan Seitz <jan.seitz@me.com>
    Co-authored-by: Andreas Gsponer <andreas.gsponer@lhep.unibe.ch>

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,6 +3,7 @@ from flask.scaffold import _endpoint_from_view_func
 from werkzeug.utils import cached_property
 
 import flask
+from flask import request
 
 flask.helpers._endpoint_from_view_func = _endpoint_from_view_func
 
@@ -17,7 +18,15 @@ api = Api(app)
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'borld'}
+
+@api.route("/ask", methods=['GET'])
+class AskToValueNet(Resource):
+    def get(self):
+        if 'id' in request.args:
+            id = int(request.args['id'])
+        else:
+            return "Error: No id field provided. Please specify an id."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
