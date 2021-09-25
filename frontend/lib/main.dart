98685929395b7f1204b6cb4873f7321e41381a71
Andreas Gsponer <andreas.gsponer@lhep.unibe.ch>
2021-09-25 20:01:13 +0200

    Revert "Merge branch 'sql_connection' of https://github.com/dominiqueroux/foitt_datahunters into sql_connection"
    This reverts commit 8ae04feb72d8ec60747b61c1842b1154e2f38b7d, reversing
    changes made to a08ebe42722762db52ff02723bbd00fa4880c1cc.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:flutter_svg/flutter_svg.dart' as svg;
+import 'package:http/http.dart' as http;
 
 import 'dart:async';
 import 'dart:convert';
 
 Future<ValueNetResponse> fetchValueNetResponse() async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/ValueNetResponses/1'),
+  );
+
   return ValueNetResponse(
       beams: "",
       potential_values_found_in_db: "",
@@ -16,7 +19,7 @@ Future<ValueNetResponse> fetchValueNetResponse() async {
       sql: "");
 }
 
-Future<ValueNetResponse> updateValueNetResponse_foitt(String query) async {
+Future<ValueNetResponse> updateValueNetResponse(String query) async {
   var url = Uri.parse(
       'https://inference.hackzurich2021.hack-with-admin.ch/api/question/hack_zurich');
 
@@ -31,45 +34,11 @@ Future<ValueNetResponse> updateValueNetResponse_foitt(String query) async {
 
   final body = {"question": query};
 
-  final response = await put(
+  final response = await http.put(
     url,
     headers: headers,
     body: jsonEncode(body), // use jsonEncode()
   );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return ValueNetResponse.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to update ValueNetResponse.');
-  }
-}
-
-Future<ValueNetResponse> updateValueNetResponse_team(String query) async {
-  var headers = {
-    'Content-Type': 'text/plain',
-    'Accept': '*/*',
-    'Host': 'localhost:5000',
-  };
-  if (query == "") {
-    query = "What is the share of electric cars in 2016 for Wetzikon?";
-  }
-  var query_json = {'question': query};
-
-  final uri = 'http://localhost:5000' + '/ask?question=' + query;
-
-  Response response = await get(Uri.parse(uri));
-  // var encoded = Uri('http://localhost:5000/hello');
-  // print(encoded);
-
-  // final response = await http.get(url);
-  print('\n\n\n\n\n');
-  print(response);
-  print(response.statusCode);
-  print('\n\n\n\n\n');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
@@ -146,7 +115,7 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Data Hunters finding insights about Switzerland'),
+          title: const Text('Update Data Example'),
         ),
         body: Container(
           alignment: Alignment.center,
@@ -170,10 +139,10 @@ class _MyAppState extends State<MyApp> {
                         onPressed: () {
                           setState(() {
                             _futureValueNetResponse =
-                                updateValueNetResponse_team(_controller.text);
+                                updateValueNetResponse(_controller.text);
                           });
                         },
-                        child: const Text('Run Query'),
+                        child: const Text('Update Data'),
                       ),
                     ],
                   );
