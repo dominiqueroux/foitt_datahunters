eb64d93ad755e86fac7294dcaa7c8d268fdb9533
Andreas Gsponer <andreas.gsponer@lhep.unibe.ch>
2021-09-25 10:08:28 +0200

    Working version of calling API + displaying data

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,55 +1,25 @@
 import 'package:flutter/material.dart';
-import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+import 'dart:async';
+import 'dart:convert';
+
+Future<ValueNetResponse> fetchValueNetResponse() async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/ValueNetResponses/1'),
+  );
+
+  return ValueNetResponse(
+      beams: "",
+      potential_values_found_in_db: "",
+      question: "",
+      question_tokenized: "",
+      result: "",
+      sem_ql: "",
+      sql: "");
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-Future<String> queryHostedValuenet(String valuenetQuery) async {
+Future<ValueNetResponse> updateValueNetResponse(String query) async {
   var url = Uri.parse(
       'https://inference.hackzurich2021.hack-with-admin.ch/api/question/hack_zurich');
 
@@ -57,92 +27,135 @@ Future<String> queryHostedValuenet(String valuenetQuery) async {
     'Content-Type': 'application/json',
     "X-API-KEY": "sjNmaCtviYzXWlS"
   };
+  if (query == "") {
+    query = "What is the share of electric cars in 2016 for Wetzikon?";
+  }
+  // String query_hardcoded = "What is the share of electric cars in 2016 for Wetzikon?";
 
-  final body = {"question": valuenetQuery};
+  final body = {"question": query};
 
-  var response = await http.put(
+  final response = await http.put(
     url,
     headers: headers,
-    body: convert.jsonEncode(body), // use jsonEncode()
+    body: jsonEncode(body), // use jsonEncode()
   );
 
   if (response.statusCode == 200) {
-    return response.body;
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return ValueNetResponse.fromJson(jsonDecode(response.body));
   } else {
-    throw Exception('Failed to interact with API');
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to update ValueNetResponse.');
   }
 }
 
-void test_query() async {
-  String response = await queryHostedValuenet(
-      "What is the share of electric cars in 2016 for Wetzikon?");
-  print(response);
+class ValueNetResponse {
+  final String beams;
+  final String potential_values_found_in_db;
+  final String question;
+  final String question_tokenized;
+  final String result;
+  final String sem_ql;
+  final String sql;
+
+  ValueNetResponse(
+      {required this.beams,
+      required this.potential_values_found_in_db,
+      required this.question,
+      required this.question_tokenized,
+      required this.result,
+      required this.sem_ql,
+      required this.sql});
+
+  factory ValueNetResponse.fromJson(Map<String, dynamic> json) {
+    return ValueNetResponse(
+      beams: json['beams'].toString(),
+      potential_values_found_in_db:
+          json['potential_values_found_in_db'].toString(),
+      question: json['question'].toString(),
+      question_tokenized: json['question_tokenized'].toString(),
+      result: json['result'].toString(),
+      sem_ql: json['sem_ql'].toString(),
+      sql: json['sql'].toString(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+void main() {
+  runApp(const MyApp());
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+  late Future<ValueNetResponse> _futureValueNetResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureValueNetResponse = fetchValueNetResponse();
   }
 
   @override
   Widget build(BuildContext context) {
-    test_query();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Update Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Click the button to query the ValueNet API (check console output)',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Update Data Example'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder<ValueNetResponse>(
+            future: _futureValueNetResponse,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(snapshot.data!.sql),
+                      TextField(
+                        controller: _controller,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter Query for ValueNet',
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _futureValueNetResponse =
+                                updateValueNetResponse(_controller.text);
+                          });
+                        },
+                        child: const Text('Update Data'),
+                      ),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+              }
+
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
