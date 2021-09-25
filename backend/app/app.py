f9656358e826cbc02799c5a5cdabfc4c40b5e496
Roland <me@rolandh.dev>
2021-09-25 13:39:47 +0200

    code with volume

--- backend/app/app.py
+++ backend/app/app.py
@@ -17,7 +17,7 @@ api = Api(app)
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'rorld'}
+        return {'hello': 'borld'}
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
