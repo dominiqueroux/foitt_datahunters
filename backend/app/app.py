883c27e4092ba8299bf7688a40907168658be4c6
webseitz <36879923+webseitz@users.noreply.github.com>
2021-09-27 09:45:17 +0200

    add more ways to get more data (#17)

--- backend/app/app.py
+++ backend/app/app.py
@@ -6,6 +6,8 @@ import flask
 from flask import request
 #from flask_sqlalchemy import SQLAlchemy
 
+
+
 import sqlalchemy
 import requests
 import json
@@ -65,38 +67,76 @@ class HelloWorld(Resource):
 @api.route("/ask-extended", methods=['GET'])
 class AskExtendedToValueNet(Resource):
     def get(self):
-        if 'question' in request.args:
-            question = request.args['question']
+        if 'question' in request.values:
+            question = request.values["question"]
         else:
-            question = "What is the share of electric cars in 2016 for Wetzikon?"
+            question = "What is the share of electric cars in 2016 for Wetzikon?" 
 
-         # Get SQL query
+        # Get SQL query
         payload = {'question': question}
         headers = {'Content-Type': 'application/json', "X-API-KEY": "sjNmaCtviYzXWlS"}
         r = requests.put("https://inference.hackzurich2021.hack-with-admin.ch/api/question/hack_zurich", headers=headers, data=json.dumps(payload)) 
 
+        # Working queries:
+        # Which municipality has the highest public transport share?
+        # What is the share of electric cars in 2016 for Wetzikon?
+
         if r.status_code == 200:
             response = {}
+
+            response["question"] = request.values["question"]
+            
 
             # Get the «standard» data
             sql_statement = r.json()["sql"]
             resultproxy = connection.execute(sql_statement)
             standard_data = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy]
             response["standard_data"] = standard_data
+            response["standard_query"] = sql_statement
+            print(sql_statement)
 
-            # Calculate extended query
-            #sql_statement = "SELECT T1.share_electric_cars FROM share_electric_cars AS T1 JOIN spatialunit AS T2 ON T1.spatialunit_id = T2.spatialunit_id WHERE T1.year = 2016 and T2.name = 'Wetzikon'       "
-            m = re.search('(SELECT )([a-zA-Z1-9._]+)( FROM )', sql_statement)
-            select_column = m.group(2)
-            splitted_query = [string.split("FROM") for string in sql_statement.split("WHERE")]
-            middle_query = splitted_query[0][1]
-            where_clause = splitted_query[1][0].split(" and ")[1]
-            year_column = splitted_query[1][0].split(" and ")[0].split(" = ")[0]
-            new_query = "SELECT AVG("+select_column+"), "+year_column+" FROM "+middle_query+" WHERE "+where_clause+" GROUP BY "+year_column
+            # Set new query
+            new_query = ""
 
-            resultproxy2 = connection.execute(new_query)
-            further_information = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy2]
-            response["further_information"] = further_information
+            # Check if query with ORDER BY xxx LIMIT 1 
+            pattern = re.search(r"(ORDER BY )([a-zA-Z1-9._]+) ((DESC)|(ASC))( LIMIT 1)", sql_statement)
+            if pattern:
+                order_by_criteria = pattern.group(2)
+                start = sql_statement.split("FROM")[0]
+                middle_part = sql_statement.split("FROM")[1].split("ORDER BY")[0]
+                new_query = start+", "+order_by_criteria+" FROM "+middle_part
+
+            # Check if FROM WHERE query
+            pattern = re.search(r"(SELECT )([a-zA-Z1-9._]+)( FROM )([a-zA-Z1-9._\s=]+)( WHERE )([a-zA-Z1-90._\s=']+)", sql_statement)
+            if pattern:
+                # sql_statement = "SELECT T1.share_electric_cars FROM share_electric_cars AS T1 JOIN spatialunit AS T2 ON T1.spatialunit_id = T2.spatialunit_id WHERE T1.year = 2016 and T2.name = 'Wetzikon'       "
+                m = re.search('(SELECT )([a-zA-Z1-9._]+)( FROM )', sql_statement)
+                select_column = m.group(2)
+                splitted_query = [string.split("FROM") for string in sql_statement.split("WHERE")]
+                
+                middle_query = splitted_query[0][1]
+                if len(splitted_query[1][0].split(" and ")) > 1:
+                    where_clause = " WHERE "+splitted_query[1][0].split(" and ")[1]
+                else:
+                    where_clause = ""
+                year_column = splitted_query[1][0].split(" and ")[0].split(" = ")[0]
+                new_query = "SELECT AVG("+select_column+"), "+year_column+" FROM "+middle_query+where_clause+" GROUP BY "+year_column
+
+            if new_query:
+                print(new_query)
+                try:
+                    resultproxy2 = connection.execute(new_query)
+                except:
+                    print("Query not callable")
+                else:
+                    further_information = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy2]
+                    response["further_information"] = further_information
+                    response["further_query"] = new_query
+                    print(further_information)
+                
+                
+
+            # SELECT T1.name FROM spatialunit AS T1 ORDER BY T1.area DESC LIMIT 1
             json_response = flask.jsonify(response)
             json_response.headers.add('Access-Control-Allow-Origin', '*')
             return json_response
